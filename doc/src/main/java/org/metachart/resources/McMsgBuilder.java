71d411b8deabe2096cf8cccd5b099fbeb4483d0e
Thorsten-K <t.kisner@web.de>
2017-09-22 14:13:05 +0200

    doc module added

---
+++ doc/src/main/java/org/metachart/resources/McMsgBuilder.java
@@ -0,0 +1,10 @@
+package org.metachart.resources;
+
+import java.io.File;
+
+public class McMsgBuilder
+{	
+	public static final String metachartPath = "utils"+File.separator+"metachart"+File.separator;
+	
+	public static final String pivot = "metachart/msg/pivot.xml";
+}
