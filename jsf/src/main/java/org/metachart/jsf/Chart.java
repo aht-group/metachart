b314aa18d6b5dc178b21c5e82ec2258183224b54
Helge Hemmer <hhemmer@web.de>
2017-11-16 15:08:09 +0100

    IDs of charts are now UUID based (since standard IDs may interfere with queries in jQuery)

---
+++ jsf/src/main/java/org/metachart/jsf/Chart.java
@@ -0,0 +1,22 @@
+package org.metachart.jsf;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UINamingContainer;
+
+@FacesComponent(value="org.metachart.jsf.Chart")
+public class Chart extends UINamingContainer
+{
+	
+	private String chartId= UUID.randomUUID().toString().replaceAll("-","");
+	
+	public String getChartId() {
+		return chartId;
+	}
+
+	public void setCodeId(String chartId) {
+		this.chartId = chartId;
+	}
+}
