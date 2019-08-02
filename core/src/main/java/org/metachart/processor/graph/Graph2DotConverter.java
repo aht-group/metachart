4b2a73b737233868854c908bf0b9a7bb326bd79c
manandhar <manandhar@nbsm19.aht-group.com>
2019-08-02 17:20:10 +0200

    to in-line ER Diagrams with UML structure

--- core/src/main/java/org/metachart/processor/graph/Graph2DotConverter.java
+++ core/src/main/java/org/metachart/processor/graph/Graph2DotConverter.java
@@ -1,7 +1,9 @@
 package org.metachart.processor.graph;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 
 import net.sf.exlp.util.io.txt.ExlpTxtWriter;
@@ -50,6 +52,14 @@ public class Graph2DotConverter
 		txtWriter.add("");
 		txtWriter.add("");
 		
+		txtWriter.add("  mode=ipsep;");
+		txtWriter.add("  model=subset;");
+		txtWriter.add("  overlap=prism;");
+		txtWriter.add("  nodesep=0.5;");
+		txtWriter.add("  equally=true;");
+		txtWriter.add("  pad=0.3;");
+		txtWriter.add("  splines=ortho;");  
+		
 		if(ratio!=null){txtWriter.add("  ratio="+ratio+";");}
 		if(ranksep!=null){txtWriter.add("  ranksep="+ranksep+";");}
 		if(overlap!=null){txtWriter.add("  overlap="+overlap+";");}
@@ -68,6 +78,9 @@ public class Graph2DotConverter
 			sb.append(" ");
 			
 			if(!e.isDirected()){sb.append("[dir=none]");}
+			else if(e.getType().equals("OneToMany")) {sb.append("[arrowhead = invempty, arrowtail=none, dir=both]");}
+			else if(e.getType().equals("ManyToOne")) {sb.append("[arrowtail = invempty, arrowhead=none, dir=both]");}
+			else if(e.getType().equals("ManyToMany")) {sb.append("[arrowtail = invempty, arrowhead=invempty, dir=both]");}
 		
 			sb.append(";").append(ls);
 		}
@@ -79,24 +92,65 @@ public class Graph2DotConverter
 	
 	private void buildNodeDefinition(Graph g)
 	{
-		for(Node n : g.getNodes().getNode())
-		{
-			StringBuffer sb = new StringBuffer();
-			sb.append("  ").append(n.getId());
-			
-			sb.append(" [");
-			sb.append("label=\""+n.getLabel()+"\",");
-			sb.append("style=filled,");
-			sb.append("fixedsize=false,");
-			sb.append(csm.getColor(n));
-			sb.append(getFontSize(n));
-			
-			
-			sb.deleteCharAt(sb.length()-1);
-			sb.append("];");
-			txtWriter.add(sb.toString());
+		Map<String, List<Node>>  mapNodesCategories = groupGraphNode(g);
+		int NodeCategoryId = 0;
+		for (Map.Entry<String, List<Node>> entry : mapNodesCategories.entrySet()) {
+			boolean skipCatagorization  = false;
+			if(entry.getKey() == "NA" || entry.getValue().size() < 2) {skipCatagorization = true;}
+			if(!skipCatagorization) {
+				StringBuffer sbCatBegin = new StringBuffer();
+				sbCatBegin.append(" ").append(" subgraph ").append(" cluster_").append(NodeCategoryId).append("{ ");				
+				txtWriter.add(sbCatBegin.toString());
+				txtWriter.add(" label =<\n" + 
+						"       <U><FONT FACE=\"cursive\" POINT-SIZE=\"30\"> "
+						+ entry.getKey() 
+						+ "</FONT></U>\n" + 
+						"     >\n" + 
+						"     labelloc=\"t\"");
+			}
+			for(Node n: entry.getValue()) {
+				txtWriter.add(nodeToDot(n));
+			}
+			if(!skipCatagorization) {txtWriter.add(" } ");}
+			NodeCategoryId++; 
 		}
-		txtWriter.add("");
+	}
+	
+	public String nodeToDot(Node n) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("  ").append(n.getId());
+		sb.append(" [");
+		sb.append("label=\""+n.getLabel()+"\",");
+		sb.append("shape=record,");
+		sb.append("style=filled,");
+		sb.append("fixedsize=false,");
+		sb.append(csm.getColor(n));
+		sb.append(getFontSize(n));
+
+		sb.deleteCharAt(sb.length()-1);
+		sb.append("];");
+		return sb.toString();
+	}
+	
+	public Map<String, List<Node>>  groupGraphNode(Graph g)
+	{
+		Map<String, List<Node>>  mapNodesCategories = new Hashtable<String,List<Node>>();
+		for(Node node : g.getNodes().getNode())
+		{
+			String subCategory  = node.getCategory();
+
+			if (subCategory == null){subCategory="NA";}else if (subCategory.length() == 0) {subCategory="NA";}
+		    if(mapNodesCategories.containsKey(subCategory)){
+		        List<Node> subCategoryList = mapNodesCategories.get(subCategory);
+		        subCategoryList.add(node);
+			}
+		    else {
+		    	List<Node> subCategoryList = new ArrayList<Node>();
+		    	subCategoryList.add(node);
+		    	mapNodesCategories.put(subCategory, subCategoryList);
+		    }
+		}
+		return mapNodesCategories;
 	}
 	
 	private String getFontSize(Node n)
@@ -106,7 +160,7 @@ public class Graph2DotConverter
 		{
 			if(n.getSize()==3)
 			{
-				sb.append("fontsize=9,");
+				sb.append("fontsize=14,");
 			}
 		}
 		return sb.toString();
