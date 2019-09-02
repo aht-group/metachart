9c8d4cd9023bedfe1cc7a3580cc0fdad6a8fe4db
Thorsten-K <t.kisner@web.de>
2019-09-02 07:43:24 +0200

    Graphs added

---
+++ util/src/main/java/org/metachart/factory/xml/graph/XmlGraphsFactory.java
@@ -0,0 +1,12 @@
+package org.metachart.factory.xml.graph;
+
+import org.metachart.xml.graph.Graphs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XmlGraphsFactory
+{
+	final static Logger logger = LoggerFactory.getLogger(XmlGraphsFactory.class);
+	
+	public static Graphs build(){return new Graphs();}
+}
