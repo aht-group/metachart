1b7e230f5be95acca137958c0b54b16614120a8a
petersen <petersen@aht-group.com>
2020-04-22 07:53:37 +0200

    timseries test string added

--- core/src/test/java/org/metachart/js/JsBackendProcessor.java
+++ core/src/test/java/org/metachart/js/JsBackendProcessor.java
@@ -5,11 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 import javax.script.ScriptException;
+
+import org.apache.commons.io.FilenameUtils;
 
 public class JsBackendProcessor 
 {
@@ -22,6 +23,26 @@ public class JsBackendProcessor
  * */
 	
 //	private ScriptEngine engine;
+	
+	private static String timeJson = "{ infile : { 'container', { "
+			+ "title: { text: 'Solar Employment Growth by Sector, 2010-2016' }, "
+			+ "subtitle: { text: 'Source: thesolarfoundation.com' }, "
+			+ "yAxis: { title: { text: 'Number of Employees' } }, "
+			+ "xAxis: { accessibility: { rangeDescription: 'Range: 2010 to 2017' } }, "
+			+ "legend: { layout: 'vertical', align: 'right', verticalAlign: 'middle' }, "
+			+ "plotOptions: { "
+			+ "series: { label: { connectorAllowed: false }, pointStart: 2010 } }, "
+			+ "series: [{ "
+			+ "name: 'Installation', data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175] }, { "
+			+ "name: 'Manufacturing', data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434] }, { "
+			+ "name: 'Sales & Distribution', data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387] }, { "
+			+ "name: 'Project Development', data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227] }, { "
+			+ "name: 'Other', data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111] }], "
+			+ "responsive: { rules: [{ "
+			+ "condition: { maxWidth: 500 }, "
+			+ "chartOptions: { legend: { layout: 'horizontal', align: 'center', verticalAlign: 'bottom' } } }] } }), "
+			+ "type: svg, "
+			+ "resources: {files: highcharts.js,modules/exporting.js,modules/export-data.js}}";
 	
 	private static String chartJson = "{\"infile\": {\"chart\": {\"type\": \"column\"}, \"title\": {\"text\": \"Stacked column chart\"},"
 			+ " \"xAxis\": {\"categories\": [\"Apples\", \"Oranges\", \"Pears\", \"Grapes\", \"Bananas\"]},"
@@ -53,7 +74,8 @@ public class JsBackendProcessor
 	
 //		http://export.highcharts.com -o /tmp/highcharts_testchart.svg
 			
-		String[] stringGet = {"curl", "-X", "POST", "http://0.0.0.0:8889", "-H", "accept: application/json", "-H", "content-type: application/json", "-d", json };
+//		String[] stringGet = {"sh", "-c", "curl", "-X", "POST", "http://localhost:8889", "-H", "accept: application/json", "-H", "content-type: application/json", "-d", json };
+		String[] stringGet = {"sh", "-c", "curl -X POST http://localhost:8889 -H accept: application/json -H content-type: application/json -d", json};
 		Stream.of(stringGet).forEach(s -> System.out.println("TEST: " + s ));
 		return stringGet;
 	}
@@ -97,8 +119,8 @@ public class JsBackendProcessor
 	{
 		JsBackendProcessor cli = new JsBackendProcessor();
 		try {
-			String response = runProcess(xPut(chartJson));
-			Files.write(Paths.get("/home/us3r/test.svg"), response.getBytes());
+			String response = runProcess(xPut(timeJson));
+			Files.write(Paths.get("C:\\DOCKER\\test.svg"), response.getBytes());
 			System.out.println(response);
 		} catch (IOException | InterruptedException e) {
 			// TODO Auto-generated catch block
