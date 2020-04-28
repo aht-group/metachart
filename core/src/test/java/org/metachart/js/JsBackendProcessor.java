d8e045402d7b76bf9b3f6d14a0ad9da0f644740f
petersen <petersen@aht-group.com>
2020-04-28 12:54:47 +0200

    fixed logger / add HttpClient (working response on windows)

--- core/src/test/java/org/metachart/js/JsBackendProcessor.java
+++ core/src/test/java/org/metachart/js/JsBackendProcessor.java
@@ -1,23 +1,37 @@
 package org.metachart.js;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.stream.Stream;
 
 import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.metachart.graph.TestGraph2DotFactory;
+import org.metachart.test.McCoreTestBootstrap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * run against docker run -d -p 8889:8080 --pull always --rm lpeters999/highcharts-export-npm:latest
+ * run against docker run -d -p 51889:8080 lpeters999/highcharts-export-npm:latest
  */
 public class JsBackendProcessor 
-{
+{	
+	final static Logger logger = LoggerFactory.getLogger(JsBackendProcessor.class);
+	
+	private HttpClient client;
 	private ScriptEngine engine;
+	
 	private static String chartJson = "{\"infile\": {\"chart\": {\"type\": \"column\"}, \"title\": {\"text\": \"Stacked column chart\"},"
 			+ " \"xAxis\": {\"categories\": [\"Apples\", \"Oranges\", \"Pears\", \"Grapes\", \"Bananas\"]},"
 			+ " \"yAxis\": {\"min\": 0,"
@@ -38,17 +52,16 @@ public class JsBackendProcessor
 	
 	public JsBackendProcessor() 
 	{
-	    engine = new ScriptEngineManager().getEngineByName("javascript");
+		client = HttpClientBuilder.create().build();
+//	    engine = new ScriptEngineManager().getEngineByName("javascript");
 	}
 	
 	private static String[] xPut(String json) 
 	{
-	
 //		curl -H "Content-Type: application/json" -X POST -d '{"infile": {"chart": {"type": "column"}, "title": {"text": "Stacked column chart"}, "xAxis": {"categories": ["Apples", "Oranges", "Pears", "Grapes", "Bananas"]}, "yAxis": {"min": 0, "title": {"text": "Total fruit consumption"}, "stackLabels": {"enabled": true, "style": {"fontWeight": "bold"}}}, "legend": {"align": "right", "x": -30, "verticalAlign": "top", "y": 25, "floating": true, "borderColor": "#CCC", "borderWidth": 1, "shadow": false}, "plotOptions": {"column": {"stacking": "normal", "dataLabels": {"enabled": true}}}, "series": [{"name": "John", "data": [5, 3, 4, 7, 2]}, {"name": "Jane", "data": [2, 2, 3, 2, 1]}, {"name": "Joe", "data": [3, 4, 4, 2, 5]}]}, "type": "svg", "resources": {"files": "highcharts.js,modules/exporting.js,modules/export-data.js"}}' http://export.highcharts.com -o /tmp/highcharts_testchart.svg
-	
 //		http://export.highcharts.com -o /tmp/highcharts_testchart.svg
 			
-		String[] stringGet = {"curl", "-X", "POST", "localhost:8889", "-H", "accept: application/json", "-H", "content-type: application/json", "-d", json };
+		String[] stringGet = {"curl", "-X", "POST", "192.168.1.20:8999", "-H", "accept: application/json", "-H", "content-type: application/json", "-d", json };
 		Stream.of(stringGet).forEach(s -> System.out.println("TEST: " + s ));
 		return stringGet;
 	}
@@ -82,24 +95,27 @@ public class JsBackendProcessor
 				+ "FULL_PATH + 'highchart-8.0.4/code/modules/exporting.js', "
 				+ "FULL_PATH + 'highchart-8.0.4/code/modules/export-data.js', "
 				+ "FULL_PATH + 'highchart-8.0.4/code/modules/accessibility.js', "
-				+ "];");
-				
+				+ "];");				
 		engine.eval("if (s.length > 0){for (i = 0; i < s.length; i++){load(s[i]);}}");
-
 	}
 	
-	public static void main(String args[]) throws ScriptException, FileNotFoundException 
+	public String httpPost(String url, String json) throws ClientProtocolException, IOException
 	{
+		HttpPost httpPost = new HttpPost(url);
+		httpPost.setHeader("Content-Type", "application/json");
+		httpPost.setHeader("accept", "application/json");
+		StringEntity stringEntity = new StringEntity(json);
+		httpPost.setEntity(stringEntity);
+		HttpResponse httpRespnse = client.execute(httpPost);
+		return EntityUtils.toString(httpRespnse.getEntity(),"UTF-8");
+	}
+	
+	public static void main(String args[]) throws ScriptException, ClientProtocolException, IOException 
+	{
+		McCoreTestBootstrap.init();
 		JsBackendProcessor cli = new JsBackendProcessor();
-		try {
-			String response = runProcess(xPut(chartJson));
-			System.out.println(response);			
-		} catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	//	cli.executeJs();
+		try {String response = runProcess(xPut(chartJson));logger.info(response);} catch (IOException | InterruptedException e) {e.printStackTrace();};
+		logger.info(cli.httpPost("http://192.168.1.20:8999/", chartJson));
+//		cli.executeJs();
 	}
 }
-
--- core/src/test/java/org/metachart/test/McCoreTestBootstrap.java
+++ core/src/test/java/org/metachart/test/McCoreTestBootstrap.java
@@ -5,23 +5,41 @@ import org.metachart.xml.McNsPrefixMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.sf.exlp.exception.ExlpConfigurationException;
 import net.sf.exlp.util.config.ConfigLoader;
+import net.sf.exlp.util.io.ExlpCentralConfigPointer;
 import net.sf.exlp.util.io.LoggerInit;
 import net.sf.exlp.util.xml.JaxbUtil;
 
 public class McCoreTestBootstrap
 {
 	final static Logger logger = LoggerFactory.getLogger(McCoreTestBootstrap.class);
+	public final static String xmlConfig = "mc-core.test/config/mc.xml";
+
+	private static Configuration config;
 	
 	public static Configuration init()
+	{
+		return init(xmlConfig);
+	}
+	
+	public static Configuration init(String configFile)
 	{		
 		LoggerInit loggerInit = new LoggerInit("log4j.xml");
-		loggerInit.addAltPath("mc-core.test/config/log4j.xml");
+		loggerInit.addAltPath("mc-core.test/config");
 		loggerInit.init();
-		JaxbUtil.setNsPrefixMapper(new McNsPrefixMapper());
-			
-		ConfigLoader.add("mc-core.test/config/mc.xml");
-		Configuration config = ConfigLoader.init();					
+		//JaxbUtil.setNsPrefixMapper(new McNsPrefixMapper());
+		
+		try
+		{
+			String cfn = ExlpCentralConfigPointer.getFile("metachart","mc").getAbsolutePath();
+			ConfigLoader.add(cfn);
+			logger.info("Using additional config in: "+cfn );
+		}
+		catch (ExlpConfigurationException e) {logger.debug("No additional "+ExlpCentralConfigPointer.class.getSimpleName()+" because "+e.getMessage());}
+		
+		ConfigLoader.add(configFile);
+		config = ConfigLoader.init();
 		logger.debug("Config and Logger initialized");
 		return config;
 	}
