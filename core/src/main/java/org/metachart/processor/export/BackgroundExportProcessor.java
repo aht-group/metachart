d3c041ee919e582430f41511a96a08f3c6cd8e98
petersen <petersen@aht-group.com>
2020-04-29 08:47:49 +0200

    add BackgroundExportProcessor + TestBackgroundExport (png+svg)

---
+++ core/src/main/java/org/metachart/processor/export/BackgroundExportProcessor.java
@@ -0,0 +1,40 @@
+package org.metachart.processor.export;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackgroundExportProcessor 
+{	
+	final static Logger logger = LoggerFactory.getLogger(BackgroundExportProcessor.class);
+
+	private HttpClient client;
+
+	private static String resources = "\"resources\": {\"files\": \"highcharts.js,modules/exporting.js,modules/export-data.js\"}}";
+	
+	public BackgroundExportProcessor() 
+	{
+		client = HttpClientBuilder.create().build();
+	}
+	
+	public String exportSVG(String url, String json, String type) throws ClientProtocolException, IOException
+	{
+		HttpPost httpPost = new HttpPost(url);
+		httpPost.setHeader("Content-Type", "application/json");
+		httpPost.setHeader("accept", "application/json");
+		String postString = "{\"infile\": " + json + ", \"type\": \""+type+"\", "+resources;
+		logger.info(postString);
+		StringEntity stringEntity = new StringEntity(postString);
+		httpPost.setEntity(stringEntity);
+		HttpResponse httpRespnse = client.execute(httpPost);
+		return EntityUtils.toString(httpRespnse.getEntity(),"UTF-8");
+	}
+}
---
+++ core/src/test/java/org/metachart/export/TestBackgroundExport.java
@@ -0,0 +1,45 @@
+package org.metachart.export;
+
+import java.io.IOException;
+
+import javax.script.ScriptException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.metachart.processor.export.BackgroundExportProcessor;
+import org.metachart.test.McCoreTestBootstrap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * run against docker run -d -p 51889:8080 lpeters999/highcharts-export-npm:latest
+ */
+public class TestBackgroundExport 
+{	
+	final static Logger logger = LoggerFactory.getLogger(TestBackgroundExport.class);
+	
+	private static String chartJson = "{\"chart\": {\"type\": \"column\"}, \"title\": {\"text\": \"Stacked column chart\"},"
+			+ " \"xAxis\": {\"categories\": [\"Apples\", \"Oranges\", \"Pears\", \"Grapes\", \"Bananas\"]},"
+			+ " \"yAxis\": {\"min\": 0,"
+			+ " \"title\": {\"text\": \"Total fruit consumption\"},"
+			+ " \"stackLabels\": {\"enabled\": true,"
+			+ " \"style\": {\"fontWeight\": \"bold\"}}},"
+			+ " \"legend\": {\"align\": \"right\", \"x\": -30, \"verticalAlign\": \"top\", \"y\": 25, \"floating\": true,"
+			+ " \"borderColor\": \"#CCC\","
+			+ " \"borderWidth\": 1, \"shadow\": false},"
+			+ " \"plotOptions\": {\"column\": {\"stacking\": \"normal\", \"dataLabels\": {\"enabled\": true}}},"
+			+ " \"series\":["
+			+ "{\"name\": \"John\", \"data\": [5, 3, 4, 7, 2]},"
+			+ " {\"name\": \"Jane\", \"data\": [2, 2, 3, 2, 1]},"
+			+ " {\"name\": \"Joe\", \"data\": [3, 4, 4, 2, 5]}"
+			+ "]}";
+		
+	public TestBackgroundExport(){}
+
+	public static void main(String args[]) throws ScriptException, ClientProtocolException, IOException 
+	{
+		McCoreTestBootstrap.init();
+		BackgroundExportProcessor cli = new BackgroundExportProcessor();
+		logger.info(cli.exportSVG("http://192.168.1.20:8999/", chartJson, "png"));
+		logger.info(cli.exportSVG("http://192.168.1.20:8999/", chartJson, "svg"));
+	}
+}
