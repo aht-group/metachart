f465b31e726ed29d6510ec063632d418a73b5c94
manandhar <manandhar@nbsm19.aht-group.com>
2020-05-08 22:42:38 +0200

    added new description

--- xml/src/main/java/org/metachart/xml/chart/Ds.java
+++ xml/src/main/java/org/metachart/xml/chart/Ds.java
@@ -30,6 +30,7 @@ import javax.xml.bind.annotation.XmlType;
  *       &lt;attribute name="color" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
  *       &lt;attribute name="symbol" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
  *       &lt;attribute name="label" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
+ *       &lt;attribute name="description" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
  *       &lt;attribute name="rangeIndex" type="{http://www.w3.org/2001/XMLSchema}int" /&gt;
  *       &lt;attribute name="colorIndex" type="{http://www.w3.org/2001/XMLSchema}int" /&gt;
  *     &lt;/restriction&gt;
@@ -64,6 +65,8 @@ public class Ds
     protected String symbol;
     @XmlAttribute(name = "label")
     protected String label;
+    @XmlAttribute(name = "description")
+    protected String description;
     @XmlAttribute(name = "rangeIndex")
     protected Integer rangeIndex;
     @XmlAttribute(name = "colorIndex")
@@ -285,6 +288,34 @@ public class Ds
 
     public boolean isSetLabel() {
         return (this.label!= null);
+    }
+
+    /**
+     * Gets the value of the description property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the value of the description property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+    public boolean isSetDescription() {
+        return (this.description!= null);
     }
 
     /**
