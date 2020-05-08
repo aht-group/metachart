f7dea9ca84f48a9a6cc04572d9371e007efc3027
manandhar <manandhar@nbsm19.aht-group.com>
2020-05-09 00:02:56 +0200

    chart json export improved

--- jsf/src/main/java/org/metachart/jsf/Chart.java
+++ jsf/src/main/java/org/metachart/jsf/Chart.java
@@ -2,15 +2,24 @@ package org.metachart.jsf;
 
 import java.util.UUID;
 
+import javax.el.MethodExpression;
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UINamingContainer;
+import javax.faces.context.FacesContext;
 
 @FacesComponent(value="org.metachart.jsf.Chart")
 public class Chart extends UINamingContainer
 {
-	
+
 	private String chartId = UUID.randomUUID().toString().replaceAll("-","");
-	
+
 	public String getChartId() {return chartId;}
 	public void setCodeId(String chartId) {this.chartId = chartId;}
+
+    public void listener()
+    {
+        FacesContext context = FacesContext.getCurrentInstance();
+        MethodExpression ajaxEventListener = (MethodExpression) getAttributes().get("jsonSaveListener");
+        ajaxEventListener.invoke(context.getELContext(), new Object[] {});
+    }
 }
