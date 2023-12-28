a451d4f8bb7b38c88933413a2a8e4a68624db0cf
Thorsten-K <t.kisner@web.de>
2023-12-28 09:23:35 +0100

    formatting improved

--- xml/src/main/java/org/metachart/xml/McNsPrefixMapper.java
+++ xml/src/main/java/org/metachart/xml/McNsPrefixMapper.java
@@ -8,9 +8,9 @@ public class McNsPrefixMapper extends NamespacePrefixMapper implements NsPrefixM
 {
     public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix)
     {
-	    	if("http://www.openfuxml.org".equals(namespaceUri) ){return "ofx";}
-	    	if("http://www.openfuxml.org/editorial".equals(namespaceUri) ){return "ed";}
-	    	if("http://www.openfuxml.org/wiki".equals(namespaceUri) ){return "wiki";}
+    	if("http://www.openfuxml.org".equals(namespaceUri) ){return "ofx";}
+    	if("http://www.openfuxml.org/editorial".equals(namespaceUri) ){return "ed";}
+    	if("http://www.openfuxml.org/wiki".equals(namespaceUri) ){return "wiki";}
         if("http://www.openfuxml.org/chart".equals(namespaceUri) ){return "chart";}
         if("http://www.openfuxml.org/table".equals(namespaceUri) ){return "table";}
         if("http://www.openfuxml.org/layout".equals(namespaceUri) ){return "layout";}
@@ -27,9 +27,9 @@ public class McNsPrefixMapper extends NamespacePrefixMapper implements NsPrefixM
 
     public String[] getPreDeclaredNamespaceUris()
     {
-	    	String[] result = new String[3];
-	    	result[2] = "http://www.openfuxml.org/chart";
-	    	result = new String[0];
+    	String[] result = new String[3];
+    	result[2] = "http://www.openfuxml.org/chart";
+    	result = new String[0];
         return result;
     }
 }
