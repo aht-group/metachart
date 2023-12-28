5784ce77cb8827aae35b4ca8c2618054914d31dc
Thorsten-K <t.kisner@web.de>
2023-12-28 09:22:46 +0100

    recompiled with CXF

--- xml/src/main/java/org/metachart/model/xml/graph/ObjectFactory.java
+++ xml/src/main/java/org/metachart/model/xml/graph/ObjectFactory.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlRegistry;
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the org.metachart.xml.graph package. 
+ * generated in the org.metachart.model.xml.graph package. 
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
@@ -23,7 +23,7 @@ public class ObjectFactory {
 
 
     /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.metachart.xml.graph
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.metachart.model.xml.graph
      * 
      */
     public ObjectFactory() {
