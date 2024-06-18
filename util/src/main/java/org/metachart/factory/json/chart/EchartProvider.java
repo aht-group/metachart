e84d0ac4838586288aafaaf67785c23a7576a549
manandhar-aht <sachin.manandhar@aht-group.com>
2024-06-19 00:25:46 +0200

    Echart timeseries data demo added

--- util/src/main/java/org/metachart/factory/json/chart/EchartProvider.java
+++ util/src/main/java/org/metachart/factory/json/chart/EchartProvider.java
@@ -12,29 +12,31 @@ import org.metachart.factory.json.chart.echart.js.demo.EchartDemoHeatmap;
 import org.metachart.factory.json.chart.echart.js.demo.EchartDemoLine;
 import org.metachart.factory.json.chart.echart.js.demo.EchartDemoSankey;
 import org.metachart.factory.json.chart.echart.js.demo.EchartDemoTime;
+import org.metachart.factory.json.chart.echart.js.demo.EchartDemoTimeSeries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EchartProvider
 {
 	final static Logger logger = LoggerFactory.getLogger(EchartProvider.class);
-	
+
 	private Writer w;
-	
+
 	public static EchartProvider instance(Writer w) {return new EchartProvider(w);}
 	public EchartProvider(Writer w)
 	{
 		this.w=w;
 	}
-	
+
 	public void demo(String type, String divId) throws IOException {demo(JsonEchartFactory.Type.valueOf(type),divId);}
 	public void demo(JsonEchartFactory.Type type, String divId) throws IOException
-	{	
+	{
 		JsonEchartFactory jfEchart = JsonEchartFactory.instance(w,JsonUtil.instance()).declare(divId,JsonHtmlFactory.build("canvas",false));
 		switch(type)
 		{
 			case line: EchartDemoLine.instance(jfEchart).demo(); break;
 			case time: EchartDemoTime.instance(jfEchart).demo(); break;
+			case timeSeries: EchartDemoTimeSeries.instance(jfEchart).demo(); break;
 			case sankey: EchartDemoSankey.instance(jfEchart).demo(); break;
 			case heatmap: EchartDemoHeatmap.instance(jfEchart).demo(); break;
 			case heatbar: EchartDemoHeatbar.instance(jfEchart).demo(); break;
--- util/src/test/java/org/metachart/factory/xhtml/chart/CliEchartDemo.java
+++ util/src/test/java/org/metachart/factory/xhtml/chart/CliEchartDemo.java
@@ -10,10 +10,6 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 import org.metachart.factory.json.chart.EchartProvider;
 import org.metachart.factory.json.chart.echart.JsonEchartFactory;
-import org.metachart.factory.json.chart.echart.js.demo.EchartDemoGraph;
-import org.metachart.factory.json.chart.echart.js.demo.EchartDemoLine;
-import org.metachart.factory.json.chart.echart.js.type.JsonEchartGraphFactory;
-import org.metachart.factory.json.chart.echart.js.type.JsonEchartLineFactory;
 import org.metachart.factory.xhtml.XhtmlEchartFactory;
 import org.metachart.test.McBootstrap;
 import org.slf4j.Logger;
@@ -24,44 +20,45 @@ import net.sf.exlp.util.xml.JDomUtil;
 public class CliEchartDemo
 {
 	final static Logger logger = LoggerFactory.getLogger(CliEchartDemo.class);
-	
+
 	private final Path path;
 	private final XhtmlEchartFactory xhfEchart;
-	
+
 	public CliEchartDemo(Configuration config)
 	{
 		xhfEchart = XhtmlEchartFactory.instance();
-		
+
 		path = Paths.get(config.getString(McBootstrap.cfgDirTmp));
 		logger.info("Wrting to "+path.toString());
 	}
-	
+
 	public void demo(JsonEchartFactory.Type type) throws IOException
-	{		
+	{
 		Element html = new Element("html");
 		html.setAttribute("lang","en");
 		html.getChildren().add(xhfEchart.head("Demo: "+type));
-		
+
 		StringWriter sw = new StringWriter();
 		EchartProvider.instance(sw).demo(type,xhfEchart.getDivCntainerId());
-		
+
 		html.getChildren().add(xhfEchart.body(sw.toString()));
 
         Document doc = new Document(html);
         doc.setDocType(new org.jdom2.DocType("html"));
-		
+
 		JDomUtil.instance().omitDeclaration(true).info(doc);
 		JDomUtil.instance().omitDeclaration(true).write(doc,path.resolve("echart-"+type.toString()+".html"));
 	}
-	
+
 	public static void main (String[] args) throws Exception
-	{		
+	{
 		Configuration config = McBootstrap.init();
 		CliEchartDemo cli = new CliEchartDemo(config);
-		
-		cli.demo(JsonEchartFactory.Type.line);
+
+		cli.demo(JsonEchartFactory.Type.timeSeries);
+		//cli.demo(JsonEchartFactory.Type.line);
 //		cli.demo(JsonEchartFactory.Type.time);
-		
+
 //		cli.demo(JsonEchartFactory.Type.heatbar);
 //		cli.demo(JsonEchartFactory.Type.heatbar);
 //		cli.demo(JsonEchartFactory.Type.graph);
