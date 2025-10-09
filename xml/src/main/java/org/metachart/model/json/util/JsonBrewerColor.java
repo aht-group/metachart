3b0fc707c331d154504104679a926ad71c3f00c0
Thorsten-K <t.kisner@web.de>
2025-10-09 11:23:03 +0200

    JsonBrewerColor added

--- xml/src/main/java/org/metachart/model/json/util/JsonBrewerColor.java
+++ xml/src/main/java/org/metachart/model/json/util/JsonBrewerColor.java
@@ -2,6 +2,8 @@ package org.metachart.model.json.util;
 
 import java.io.Serializable;
 
+import org.metachart.model.json.graph.mc.JsonCategory;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class JsonBrewerColor implements Serializable
@@ -10,25 +12,18 @@ public class JsonBrewerColor implements Serializable
 	
 	@JsonProperty("id")
 	private String id;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
+	public String getId() {return id;}
+	public void setId(String id) {this.id = id;}
 	
+	@JsonProperty("category")
+	private JsonCategory category;
+	public JsonCategory getCategory() {return category;}
+	public void setCategory(JsonCategory category) {this.category = category;}
+
 	@JsonProperty("description")
 	private String description;
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
+	public String getDescription() {return description;}
+	public void setDescription(String description) {this.description = description;}
 	
 	@JsonProperty("maximumColorCount")
 	private Integer maximumColorCount;
